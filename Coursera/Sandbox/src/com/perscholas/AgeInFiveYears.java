90b8185bbcd9c22b960fbf6454da1d68f3ab50d5
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-05 19:50:10 -0600

    Update AgeInFiveYears.java

--- Coursera/Sandbox/src/com/perscholas/AgeInFiveYears.java
+++ Coursera/Sandbox/src/com/perscholas/AgeInFiveYears.java
@@ -1,29 +1,24 @@
-package com.perscholas;
+package com.perscholas; // package of the project
 
-import java.util.Scanner;
+import java.util.Scanner; //scanner utility created
 
-public class AgeInFiveYears {
+public class AgeInFiveYears // class of the package com.perscholas
+{
 
-	public static void main(String[] args) {
-		// create object of scanner class for input by user.
-				Scanner input = new Scanner(System.in);
+	public static void main(String[] args) // main method
+{
+	Scanner input = new Scanner(System.in); // create object of scanner class for input by user
+	
+	System.out.println("Hi there. What's your name?"); // ask for user name
+	String name = input.nextLine(); // define string variable for users name
+	
+	System.out.println("Hi " + name + "! My name is Zaki. Nice to meet you. How old are you?"); // ask user for age
 				
-				// User enters the name here.
-				System.out.println("Hi there. What's your name?");
-
-				// define variables and characters
-				
-				String name = input.nextLine(); //looks at the next String or variable
-				System.out.println("Hi " + name + "! My name is Zaki. Nice to meet you. How old are you?");
-				
-				//user enters age here. age is calculated +5 and -5
-				int age = input.nextInt();
-				int age2 = age + 5;
-				int age3 = age - 5;		
-				
-				//output what you want to see
-				System.out.println("Did you know that in 5 years you will be " + age2 + " years old? And 5 years ago you were " + age3 + "! Imagine that!");
-				
-	}
-
+	int age = input.nextInt(); // define integer variable for users age
+	int age2 = age + 5; // calculate user age + 5
+	int age3 = age - 5; // calculate user age - 5
+		
+	//output user age after variables are calculated	
+	System.out.println("Did you know that in 5 years you will be " + age2 + " years old? And 5 years ago you were " + age3 + "! Imagine that!");
+}
 }
