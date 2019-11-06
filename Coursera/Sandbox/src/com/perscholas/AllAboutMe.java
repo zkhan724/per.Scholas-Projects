56b73f5f1bd82ba61d1abfb9b0b48ea0ae10aabc
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-05 20:10:10 -0600

    Update AllAboutMe.java

--- Coursera/Sandbox/src/com/perscholas/AllAboutMe.java
+++ Coursera/Sandbox/src/com/perscholas/AllAboutMe.java
@@ -1,42 +1,42 @@
-package com.perscholas;
+package com.perscholas; // package of Project Sandbox
 
-import java.util.Scanner;
+import java.util.Scanner; // Scanner object created
 
-public class AllAboutMe {
+public class AllAboutMe // class of package com.perscholas
+{
 
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("What is your name?");
-		
-		String name = input.nextLine();
-		System.out.println("How old are you?");
-		
-		String age = input.nextLine();
-		System.out.println("How tall are you?");
-		
-		String height = input.nextLine();
-		int num1 = Integer.parseInt(height);//converting string to an integer
-		int num2 = (int) (num1 * 30.48);//taking the converted integer and doing the centimeter calculation
-		System.out.println("What color are your eyes?");
-		
-		String eyes = input.nextLine();
-		System.out.println("What color are your teeth?");
-		
-		String teeth = input.nextLine();
-		System.out.println("What color is your hair?");
-		
-		String hair = input.nextLine();
-		
-		System.out.println("\nLet's talk about " + name + 
-		"\nThey're " + num2 + " centimeters tall." + 
-		"\nThey're " + age + " years old." + 
-		"\nHuh, that's older than I expected..." +
-		"\nThey have " + eyes + " eyes" + " and " + hair + " hair." +
-		"\nTheir teeth are usually " + teeth + ", but it depends on the coffee." +
-		"\nAlright, this is pretty boring. Let's stop talking about " + name + "."); 
+	public static void main(String[] args) // main method
+{
+	Scanner input = new Scanner(System.in); // scanner object created for user input
+	System.out.println("What is your name?"); // ask user for name
+	String name = input.nextLine(); // capture and define string variable for name
 
-	}
+	System.out.println("How old are you?"); // ask user for age
+	String age = input.nextLine(); // capture and define string variable for age
+	
+	System.out.println("How tall are you?"); // ask user for height
+	String height = input.nextLine(); // capture and define string variable for height
+	int num1 = Integer.parseInt(height); //converting a string variable to an integer variable for height
+	int num2 = (int) (num1 * 30.48);// height conversion to centimeters 
+		
+	System.out.println("What color are your eyes?"); // ask user for eye color
+	String eyes = input.nextLine(); // capture and define string variable for eye color
+	
+	System.out.println("What color are your teeth?"); // ask user for teeth color
+	String teeth = input.nextLine(); // capture and define string variable for teeth color
+	
+	System.out.println("What color is your hair?");	// ask user for hair color
+	String hair = input.nextLine(); // capture and define string variable for hair color
+		
+	// output story based upon user	
+	System.out.println("\nLet's talk about " + name + 
+	"\nThey're " + num2 + " centimeters tall." + 
+	"\nThey're " + age + " years old." + 
+	"\nHuh, that's older than I expected..." +
+	"\nThey have " + eyes + " eyes" + " and " + hair + " hair." +
+	"\nTheir teeth are usually " + teeth + ", but it depends on the coffee." +
+	"\nAlright, this is pretty boring. Let's stop talking about " + name + ".");
+}
 
 }
 
