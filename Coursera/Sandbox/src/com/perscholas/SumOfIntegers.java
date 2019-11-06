11296b2a8194aa57ca7788f317a8aabbf28e47f6
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-06 09:30:12 -0600

    Update SumOfIntegers.java

--- Coursera/Sandbox/src/com/perscholas/SumOfIntegers.java
+++ Coursera/Sandbox/src/com/perscholas/SumOfIntegers.java
@@ -6,15 +6,16 @@ public class SumOfIntegers {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Give me your first number:");
-		int num1 = input.nextInt();
-		System.out.println("Give me your second number:");
-		double num2 = input.nextDouble();
-		double sum = num1 * num2;
+		Scanner input = new Scanner(System.in); // creates scanner object to allow user input
+		System.out.println("Give me your first number:"); // ask user for the first number
 		
-		System.out.println("The Product is " + sum + ".");
+		int num1 = input.nextInt(); // define integer variable from user input
+		System.out.println("Give me your second number:"); // ask user for the second number
 		
+		double num2 = input.nextDouble(); // define integer variable from user input
+		double sum = num1 * num2; // calculate the sum of num1 & num2
+		
+		System.out.println("The Product is " + sum + "."); //output the sum		
 
 	}
 
