e1e50c8b508bec0453c2f5e57046f5167aaed43d
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 11:38:15 -0600

    Update AgeInFiveYears.java

--- Coursera/Sandbox/src/com/perscholas/AgeInFiveYears.java
+++ Coursera/Sandbox/src/com/perscholas/AgeInFiveYears.java
@@ -1,24 +1,32 @@
-package com.perscholas; // package of the project
+package com.perscholas;
 
-import java.util.Scanner; //scanner utility created
+// This program calculates user age by +-5 years.
 
-public class AgeInFiveYears // class of the package com.perscholas
+import java.util.Scanner;
+
+public class AgeInFiveYears
 {
 
-	public static void main(String[] args) // main method
+	public static void main(String[] args)
 {
-	Scanner input = new Scanner(System.in); // create object of scanner class for input by user
+	// create object of scanner class for input by user
+	Scanner input = new Scanner(System.in);
 	
-	System.out.println("Hi there. What's your name?"); // ask for user name
-	String name = input.nextLine(); // define string variable for users name
+	// ask for user's name
+	System.out.println("Hi there. What's your name?");
 	
-	System.out.println("Hi " + name + "! My name is Zaki. Nice to meet you. How old are you?"); // ask user for age
+	// define string variable for users name
+	String name = input.nextLine(); 
+	
+	// ask user for age
+	System.out.println("Hi " + name + "! My name is Zaki. Nice to meet you. How old are you?");
 				
-	int age = input.nextInt(); // define integer variable for users age
-	int age2 = age + 5; // calculate user age + 5
-	int age3 = age - 5; // calculate user age - 5
+	// define integer variable for users age, and calculate age in 5 and past 5 years
+	int age = input.nextInt();
+	int age2 = age + 5; 
+	int age3 = age - 5; 
 		
-	//output user age after variables are calculated	
+	// print results	
 	System.out.println("Did you know that in 5 years you will be " + age2 + " years old? And 5 years ago you were " + age3 + "! Imagine that!");
 }
 }
