a6c583b806d202d4f701b176c064dbe4bf14b1b3
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 10:50:35 -0600

    Update InchesToMetersCalc.java

--- Coursera/Sandbox/src/com/perscholas/InchesToMetersCalc.java
+++ Coursera/Sandbox/src/com/perscholas/InchesToMetersCalc.java
@@ -1,21 +1,26 @@
 package com.perscholas; 
 
+// This program converts height from inches to meters.
+
 import java.util.Scanner;
 
 public class InchesToMetersCalc {
 
 	public static void main(String[] args) {
+		
+		// create an object to receive input from the user.
 		Scanner input = new Scanner(System.in);
 		
-		// User enters value here.
-		System.out.println("Input a value for inch: ");
+		// User enters height in inches here.
+		System.out.println("What is your height in inches? ");
 
-		// define variables and characters
+		// define height from user and calculate to meters
 		double num1 = input.nextDouble();
 		double num2 = 0.0254;
+		double num3 = num1 * 0.0254;
 			
-		//output what you want to see
-		System.out.println(num1 * num2);
+		// print out height in meters
+		System.out.println("Your height is " + num3 + " meters!");
 
 	}
 
