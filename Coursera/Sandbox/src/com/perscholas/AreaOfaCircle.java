a4a021ed90976e6452019103815133fc1ada7e4c
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 11:29:37 -0600

    Update AreaOfaCircle.java

--- Coursera/Sandbox/src/com/perscholas/AreaOfaCircle.java
+++ Coursera/Sandbox/src/com/perscholas/AreaOfaCircle.java
@@ -1,17 +1,25 @@
 package com.perscholas;
 
+//This program calculates the area of a circle based upon user input of radius.
+
 import java.util.Scanner;
 
 public class AreaOfaCircle {
 
-	public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Radius of a Circle: ");
+	public static void main(String[] args) 
+	{
+        	// create scanner object
+		Scanner input = new Scanner(System.in);
+		
+		// ask user for the radius of a circle
+        	System.out.print("Enter the Radius of a Circle: ");
+        	
+		// define the radius, and calculate the are using Math.PI
+        	double radius = input.nextDouble();
+        	double area = Math.PI * (radius * radius);      
         
-        double radius = input.nextDouble();
-        double area = Math.PI * (radius * radius);      
-        
-        System.out.println("Area = " + area);
+		// Print results
+        	System.out.println("Area = " + area);
 
 	}
 
