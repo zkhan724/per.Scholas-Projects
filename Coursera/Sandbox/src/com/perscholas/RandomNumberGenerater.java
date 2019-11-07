b3ccac1e6b3a3bbbc2489fb470b4be518fb6861f
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 09:10:30 -0600

    Update RandomNumberGenerater.java

--- Coursera/Sandbox/src/com/perscholas/RandomNumberGenerater.java
+++ Coursera/Sandbox/src/com/perscholas/RandomNumberGenerater.java
@@ -9,24 +9,29 @@ public class RandomNumberGenerater
 	Scanner input = new Scanner(System.in);
 		
 	/* generate a random integer from 0-10. Add +1 to include 10 because Math.random generates number from 0-1 only. 
-	* putting an int before math.random makes it into an integer instead of a double*/
+	putting an int before math.random makes it into an integer instead of a double
+	*/
 		
 	int num1 = (int) ((Math.random() * 50) +1); //
 	int num2 = (int) ((Math.random() * 50) +1);
+	
+	// ASK USER TO ANSWER EQUATION
+	System.out.println("What is " + num1 + " + " + num2 + "?");
 		
-	System.out.println("What is " + num1 + " + " + num2 + "?"); // ASK USER TO ANSWER EQUATION
+	//STORE THE USERS RESPONSE TO CONFIRM IF IT IS CORRECT
+	int response = input.nextInt();
 		
-	int response = input.nextInt(); //STORE THE USERS RESPONSE TO CONFIRM IF IT IS CORRECT
-		
-	input.close(); //CLOSE SCANNER OBJECT AFTER YOU GET USER RESPONSE
-		
-	if(response == num1 + num2) //TEST THE USERS RESPONSE TO CONFIRM IF IT IS CORRECT (DONT NEED SEMICOLON)
-		{
-		System.out.println("You are correct!"); //PRINT OUT STATEMENT IF USERS ANSWER IS CORRECT
-		}
+	//CLOSE SCANNER
+	input.close();
+	
+	//TEST THE USERS RESPONSE TO CONFIRM IF STATEMENT IS TRUE
+	if(response == num1 + num2)
+	{
+	System.out.println("You are correct!");
+	}
 	else 
-		{
-		System.out.println("Try again!"); //PRINT OUT A STATEMENT IS USERS ANSWER IS INCORRECT
-		}
+	{
+	System.out.println("Try again!"); 
+	}
   }
 }
