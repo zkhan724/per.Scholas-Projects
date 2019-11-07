36c0b674aa29410d859e0abfb1546fc5ccd86a31
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 09:41:02 -0600

    Update ProductOfTwoNumbers.java

--- Coursera/Sandbox/src/com/perscholas/ProductOfTwoNumbers.java
+++ Coursera/Sandbox/src/com/perscholas/ProductOfTwoNumbers.java
@@ -1,24 +1,30 @@
 package com.perscholas;
 
+// This program gives the Product of Two Numbers.
+
 import java.util.Scanner;
 
 public class ProductOfTwoNumbers {
 
 	public static void main(String[] args) {
+		
+		// Scanner object created for user input
 		Scanner input = new Scanner(System.in);
 		
-		// User enters value here.
-		System.out.println("Input first number: ");// you get first number right here (input)
+		// User inputs first number.
+		System.out.println("Input first number: ");
 
-		// define variables and characters
+		// Define first number.
 		double num1 = input.nextDouble();
+		
+		// User inputs second number.
 		System.out.println("Input second number: ");
 		
+		// Define second number and calculate both numbers from user
 		double num2 = input.nextDouble();
 		double num3 = num1 * num2;
 		
-			
-		//output what you want to see
+		//Print results.
 		System.out.println("Expected Output: " + num3);
 
 	}
