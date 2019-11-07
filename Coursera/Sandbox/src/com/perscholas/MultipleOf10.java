1965cebee6b1909abf060dbad92b2e88a50f1376
zkhan724 <49889293+zkhan724@users.noreply.github.com>
2019-11-07 10:31:48 -0600

    Update MultipleOf10.java

--- Coursera/Sandbox/src/com/perscholas/MultipleOf10.java
+++ Coursera/Sandbox/src/com/perscholas/MultipleOf10.java
@@ -1,17 +1,26 @@
 package com.perscholas;
 
+// This program gives muliplies a number from the range of 1-10.
+
 import java.util.Scanner;
 
 public class MultipleOf10 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in); //function for user input
 		
-		System.out.println("Enter a value: "); // user input
+		// create an object to receive input from the user.
+		Scanner input = new Scanner(System.in);
+		
+		//ask user to enter a value from 1-10
+		System.out.println("Enter a value from 1-10:"); 
+		
+		// define value from user input
 		int num1 = input.nextInt();
 		
-		for (int i = 0; i< 10; i++) // build the loop when you increment the integer
-			
+		// increment the integer
+		for (int i = 0; i< 10; i++)
+		
+		// print results
 		System.out.println(num1 + " x " + (i+1) + " = " + (num1 * (i+1)));
 
 	}
